b942b33bef1334efd0a57097de3844884e7e93a9
Kirill Asyamolov <kirilla@playtika.com>
2020-03-22 00:41:54 +0300

    fix bug spells reading

--- src/spells/SpellHandler.hx
+++ src/spells/SpellHandler.hx
@@ -28,54 +28,56 @@ class SpellHandler extends HandlerBase<SpellId, Spell> implements IHandlerBase {
     }
 
     override public function loadLegacyData(dataSize:Int):Array<Dynamic> {
-        function read(combat:Bool, ability:Bool, legacyData:Array<Dynamic>, parseObject:Array<Dynamic>) {
+        function read(combat:Bool, ability:Bool, legacyData:Array<Dynamic>, groupParseObject:Array<Dynamic>) {
             var readSchool = function(data:Dynamic, schoolName:String, value:Bool) {
                 if (value) {
                     Reflect.setField(data, schoolName, value);
                 }
             }
 
-            var lineNode = {};
-            var id = legacyData.length;
-            var pos:Int = 0;
+            for (parseObject in groupParseObject) {
+                var lineNode = {};
+                var id = legacyData.length;
+                var pos:Int = 0;
 
-            Reflect.setField(lineNode, "index", id);
-            Reflect.setField(lineNode, "type", ability ? "ability" : (combat ? "combat" : "adventure"));
+                Reflect.setField(lineNode, "index", id);
+                Reflect.setField(lineNode, "type", ability ? "ability" : (combat ? "combat" : "adventure"));
 
-            Reflect.setField(lineNode, "name", parseObject[pos++]);
-            pos++;
+                Reflect.setField(lineNode, "name", parseObject[pos++]);
+                pos++;
 
-            Reflect.setField(lineNode, "level", parseObject[pos++]);
+                Reflect.setField(lineNode, "level", parseObject[pos++]);
 
-            var schools = {};
-            readSchool(schools, "earth", parseObject[pos++]);
-            readSchool(schools, "water", parseObject[pos++]);
-            readSchool(schools, "fire", parseObject[pos++]);
-            readSchool(schools, "air", parseObject[pos++]);
+                var schools = {};
+                readSchool(schools, "earth", parseObject[pos++]);
+                readSchool(schools, "water", parseObject[pos++]);
+                readSchool(schools, "fire", parseObject[pos++]);
+                readSchool(schools, "air", parseObject[pos++]);
 
-            var levelsObj = {};
-            var costs = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
-            Reflect.setField(lineNode, "power", parseObject[pos++]);
-            var powers = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
+                var levelsObj = {};
+                var costs = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
+                Reflect.setField(lineNode, "power", parseObject[pos++]);
+                var powers = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
 
-            var chancesObj = {};
-            for (i in 0...GameConstants.F_NUMBER) {
-                Reflect.setField(chancesObj, StringConstants.TOWN_TYPE[i], parseObject[pos++]);
+                var chancesObj = {};
+                for (i in 0...GameConstants.F_NUMBER) {
+                    Reflect.setField(chancesObj, StringConstants.TOWN_TYPE[i], parseObject[pos++]);
+                }
+                Reflect.setField(lineNode, "gainChance", chancesObj);
+
+                var aiVals = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
+
+                var descriptions:Array<String> = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
+
+                pos++; //ignore attributes. All data present in JSON
+
+                for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) {
+                    var level = {description: descriptions[i], cost: costs[i], power: powers[i], aiValue: aiVals[i]};
+                    Reflect.setField(levelsObj, LEVEL_NAMES[i], level);
+                }
+
+                legacyData.push(lineNode);
             }
-            Reflect.setField(lineNode, "gainChance", chancesObj);
-
-            var aiVals = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
-
-            var descriptions:Array<String> = [for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) parseObject[pos++]];
-
-            pos++; //ignore attributes. All data present in JSON
-
-            for (i in 0...GameConstants.SPELL_SCHOOL_LEVELS) {
-                var level = {description: descriptions[i], cost: costs[i], power: powers[i], aiValue: aiVals[i]};
-                Reflect.setField(levelsObj, LEVEL_NAMES[i], level);
-            }
-
-            legacyData.push(lineNode);
         }
 
 
@@ -83,9 +85,9 @@ class SpellHandler extends HandlerBase<SpellId, Spell> implements IHandlerBase {
 
         var parser = FileCache.instance.getConfig("DATA/SPTRAITS.TXT");
 
-        read(false, false, legacyData, parser); //read adventure map spells
-        read(true, false, legacyData, parser); //read battle spells
-        read(true, true, legacyData, parser);//read creature abilities
+        read(false, false, legacyData, parser[0]); //read adventure map spells
+        read(true, false, legacyData, parser[1]); //read battle spells
+        read(true, true, legacyData, parser[2]);//read creature abilities
 
         //TODO: maybe move to config
         //clone Acid Breath attributes for Acid Breath damage effect
